f78574e271c25b31332c73e4c8d83be2f826e993
José Manuel Cózar <josemanueltrafu@gmail.com>
2025-06-09 00:50:55 +0200

    Added routes and icons

--- src/router.jsx
+++ src/router.jsx
@@ -4,6 +4,10 @@ import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
 import GroupsList from "./pages/GroupsList";
+import CreateGroup from "./pages/CreateGroup";
+import EditProfile from "./pages/EditProfile";
+import AddUsersToGroup from "./pages/AddUsersToGroup";
+import ErrorPage from "./pages/ErrorPage";
 
 const AppRouter = () => {
   return (
@@ -15,6 +19,17 @@ const AppRouter = () => {
       <Route path="/reset-password" element={<ResetPassword />} />
 
       <Route path="/groups" element={<GroupsList />} />
+      <Route path="/groups/create-group" element={<CreateGroup />} />
+      <Route
+        path="/groups/:groupUUID/add-users"
+        element={<AddUsersToGroup />}
+      />
+
+      <Route path="/edit-profile" element={<EditProfile />} />
+
+      <Route path="/error" element={<ErrorPage />} />
+
+      <Route path="*" element={<Navigate to={"/error"} />} />
     </Routes>
   );
 };
